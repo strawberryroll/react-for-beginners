9de31139986dfb67099b87835610a49b119ff3b6
ahyeon <nuyha234@gmail.com>
2025-02-12 17:35:45 +0900

    useEffect dependency 변경에 따른 동작 확인

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    };
+    console.log("i run all the time");
+    useEffect(() => console.log("i run only once."), []);
+    useEffect(() => {
+        console.log("i run when 'keyword' changes.", keyword);
+    }, [keyword]);
+    useEffect(() => console.log("i run when 'counter' changes."), [counter]);
+
     return (
         <div>
-            <h1>Welcome back!</h1>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
         </div>
     );
 }
