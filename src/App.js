b4f732b1d75eb980c0575301bde1cd39b06b2940
ahyeon <nuyha234@gmail.com>
2025-02-12 17:37:11 +0900

    useEffect cleanup 학습을 위한 코드 작성

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => {
-        setKeyword(event.target.value);
-    };
-    console.log("i run all the time");
-    useEffect(() => console.log("i run only once."), []);
+function Hello() {
     useEffect(() => {
-        console.log("i run when 'keyword' changes.", keyword);
-    }, [keyword]);
-    useEffect(() => console.log("i run when 'counter' changes."), [counter]);
+        console.log("Hi");
+        return () => console.log("Bye");
+    }, []);
+
+    return <h1>Hello</h1>;
+}
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
 
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
